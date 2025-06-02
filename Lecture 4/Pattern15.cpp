afd6db9d0a6b22ecc29b85410b565620a989f212
Navmesh Srivastava <navmeshsrivastav815@gmail.com>
2025-06-02 22:22:49 +0530

    Pattern 13, 15 and 15

---
+++ Lecture 4/Pattern15.cpp
@@ -0,0 +1,32 @@
+/*
+D
+C   D
+B   C   D
+A   B   C   D
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i<=n)
+    {
+        char ch = 'A' + n - i;
+        int j = 1;
+        while (j<=i)
+        {
+            cout << ch;
+            ch++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    
+
+    return 0;
+}
